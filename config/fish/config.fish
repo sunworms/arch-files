d7a5c755e32467b0ff98769e6591eb0901c6067e
Sunny <sunnybhowmick0310@gmail.com>
2026-05-04 19:21:26 +0530

    stopped fish cursor from blinking

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,4 +7,9 @@ if status is-interactive
 
     set -U fish_greeting
     set -g fish_key_bindings fish_vi_key_bindings
+    set -g fish_cursor_default block
+    set -g fish_cursor_insert line
+    set -g fish_cursor_visual_block block
+    set -g fish_cursor_replace_one underscore
+    set -g fish_cursor_unknown block
 end
