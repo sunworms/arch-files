1df7e4d66f6e983aebf1059368508f3e8d6bae63
Sunny <sunnybhowmick0310@gmail.com>
2026-05-04 23:23:09 +0530

    made fish faster(?)

--- config/dot-config/fish/config.fish
+++ config/dot-config/fish/config.fish
@@ -1,11 +1,5 @@
 if status is-interactive
-    abbr --add -- grep 'grep --color=auto'
-    abbr --add -- l 'ls -alh'
-    abbr --add -- ll 'ls -l'
-    abbr --add -- ls 'ls --color=tty'
-    abbr --add -- lg lazygit
-
-    set -U fish_greeting
+    set -g fish_greeting
     set -g fish_key_bindings fish_vi_key_bindings
     set -g fish_cursor_default block
     set -g fish_cursor_insert line
---
+++ scripts/setup-abbrs.fish
@@ -0,0 +1,7 @@
+#!/usr/bin/env fish
+
+abbr --add -- grep 'grep --color=auto'
+abbr --add -- l 'ls -alh'
+abbr --add -- ll 'ls -l'
+abbr --add -- ls 'ls --color=tty'
+abbr --add -- lg lazygit
