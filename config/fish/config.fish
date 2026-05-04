c97526a1580f64889aeeee5bd2a4a21969dbf25d
Sunny <sunnybhowmick0310@gmail.com>
2026-05-04 18:37:21 +0530

    moved env variables to niri file

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,16 +1,3 @@
-if status is-login
-    set -gx ELECTRON_OZONE_PLATFORM_HINT auto
-    set -gx QT_AUTO_SCREEN_SCALE_FACTOR 1
-    set -gx QT_WAYLAND_DISABLE_WINDOWDECORATION 1
-    set -gx _JAVA_AWT_WM_NONREPARENTING 1
-    set -gx EDITOR hx
-    set -gx SUDO_EDITOR hx
-    set -gx VISUAL hx
-    set -gx TERMINAL kitty
-    set -gx GTK_THEME adw-gtk3
-    set -gx QT_QPA_PLATFORMTHEME gtk3
-    set -gx fish_user_paths /home/sunny/.local/bin
-end
 if status is-interactive
     abbr --add -- grep 'grep --color=auto'
     abbr --add -- l 'ls -alh'
