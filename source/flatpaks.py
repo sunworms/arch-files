a41df4bbf4569034865af05157ac0230afc30d75
Sunny <sunnybhowmick0310@gmail.com>
2026-05-12 00:18:50 +0530

    added flatpak and sober

---
+++ source/flatpaks.py
@@ -0,0 +1,3 @@
+import decman
+
+decman.flatpak.packages |= {"org.vinegarhq.Sober"}
--- source/source.py
+++ source/source.py
@@ -1,6 +1,15 @@
 import decman
 
+decman.execution_order = [
+    "files",
+    "pacman",
+    "aur",
+    "flatpak",
+    "systemd",
+]
+
 import packages
 import files
 import services
 import symlinks
+import flatpaks
