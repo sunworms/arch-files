a2b99ff2520425538959f97e43094f407b125dd4
Sunny <sunnybhowmick0310@gmail.com>
2026-05-12 18:27:43 +0530

    stopped fish cursor blinking

--- config/dot-config/fish/config.fish
+++ config/dot-config/fish/config.fish
@@ -8,6 +8,10 @@ if status is-interactive
     abbr --add -- ls 'ls --color=tty'
     abbr --add -- lg lazygit
 
-    set -U fish_greeting
+    set -g fish_greeting
     set -g fish_key_bindings fish_vi_key_bindings
+    set fish_cursor_default block
+    set fish_cursor_insert line
+    set fish_cursor_replace_one underscore
+    set fish_cursor_unknown block
 end
