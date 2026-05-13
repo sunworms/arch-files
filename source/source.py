3b03aa8c765252d65e9a01d38e1a7489d8f1db65
Sunny <sunnybhowmick0310@gmail.com>
2026-05-13 23:26:03 +0530

    remove flatpak

--- source/source.py
+++ source/source.py
@@ -1,15 +1,6 @@
 import decman
 
-decman.execution_order = [
-    "files",
-    "pacman",
-    "aur",
-    "flatpak",
-    "systemd",
-]
-
 import packages
 import files
 import services
 import symlinks
-import flatpaks
